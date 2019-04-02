204b9c696155eb858c8469f16777616922577e41
Marc Verdaguer Ginestera <marc.verdaguer@getinge.com>
2019-04-02 13:11:56 +0200

    Adding redux-devtools-extension configuration.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
@@ -7,5 +8,13 @@ const initialState = {};
 const middleware = [thunk];
 
 export default function makeStore() {
-  return createStore(reducers, initialState, applyMiddleware(...middleware));
+  return createStore(
+    reducers,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+  );
 }
