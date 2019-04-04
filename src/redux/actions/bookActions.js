303de59a90d723b9e3561b8a197df1d9a99e420f
Marc Verdaguer Ginestera <marc.verdaguer@getinge.com>
2019-04-04 13:29:28 +0200

    Moving orient-express url to an .env file.

--- src/redux/actions/bookActions.js
+++ src/redux/actions/bookActions.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { FETCH_BOOKS } from './types';
 
+const orientExpressBaseUrl = process.env.REACT_APP_ORIENT_EXPRESS_URL;
+
 export const fetchBooks = (inputSearch, dispatch) => () => {
   axios
     .get(
-      `http://localhost:8081/search-book?title=%22${inputSearch}%22&from=1&to=3`,
+      `${orientExpressBaseUrl}/search-book?title=%22${inputSearch}%22&from=1&to=3`,
     )
     .then(res => {
       dispatch({
